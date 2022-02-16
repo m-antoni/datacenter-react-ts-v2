88a2ba3aec60199adea837a36ebf2a4e4f87a2fd
unknown <luke@bluehive.com.ph>
2022-02-16 19:40:25 +0800

    add sentry

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,18 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './store';
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://1efbcb1b91954ca7b6219fce16a11678@o1140968.ingest.sentry.io/6205276",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
